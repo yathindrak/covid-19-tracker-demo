36299c6c08fdf57ca5c827d23a55ae81ccb5956b
Yathindra Kodithuwakku <32919513+yathindrakodithuwakku@users.noreply.github.com>
2022-07-26 17:36:23 +0530

    make url a placeholder

--- src/hooks/useStatistics.tsx
+++ src/hooks/useStatistics.tsx
@@ -7,7 +7,7 @@ export function useStatistics(
 ) {
   const { data, error } = useSWR<HttpResponse, HttpError>(
     shouldFetch
-      ? `https://1d8acbf1-e34a-4cb3-a9f5-9690a2b6d67b-prod.e1-us-east-azure.choreoapis.dev/jgdm/covid19-statistics/1.0.0/stats/usa`
+      ? `<CHOREO_API>`
       : null,
     fetcher
   );
